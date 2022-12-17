669268faf1e647cb9097f5fb3eca6e5842da9769
amiralam0 <sharpprogrammer2018@gmail.com>
2022-12-17 01:47:05 -0800

    d

---
+++ cmd/main/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"testproject/pkg/routes"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	r := mux.NewRouter()
+	routes.RegisterBookStoreRoutes(r)
+	http.Handle("/", r)
+	log.Fatal(http.ListenAndServe(":8080", r))
+}
